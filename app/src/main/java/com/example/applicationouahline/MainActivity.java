9ee1dab6c3d5592ff905b9e8173076e25f8b655b
ouahlineomaima <ouahline.omaima@ine.inpt.ma>
2022-12-31 01:50:49 +0100

    Question 8

--- app/src/main/java/com/example/applicationouahline/MainActivity.java
+++ app/src/main/java/com/example/applicationouahline/MainActivity.java
@@ -2,13 +2,14 @@ package com.example.applicationouahline;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.provider.AlarmClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button bouton_implicite, bouton_explicite;
+    Button bouton_implicite, bouton_explicite, setbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
 
         bouton_explicite = findViewById(R.id.button);
         bouton_implicite = findViewById(R.id.button2);
+        setbtn = findViewById(R.id.setbtn);
 
         bouton_explicite.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,6 +36,19 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(Intent.ACTION_VIEW);
                 intent.setData(Uri.parse("http://www.inpt.ac.ma"));
                 startActivity(intent);
+            }
+        });
+
+        setbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AlarmClock.ACTION_SET_ALARM)
+                        .putExtra(AlarmClock.EXTRA_MESSAGE, "alarme tp")
+                        .putExtra(AlarmClock.EXTRA_HOUR, 10)
+                        .putExtra(AlarmClock.EXTRA_MINUTES, 25);
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }
             }
         });
     }
