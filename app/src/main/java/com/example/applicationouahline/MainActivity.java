4908d7dd93d89cea662f0b2c2caa59a5e9879723
ouahlineomaima <ouahline.omaima@ine.inpt.ma>
2022-12-31 03:00:07 +0100

    Question1-2-3-4

--- app/src/main/java/com/example/applicationouahline/MainActivity.java
+++ app/src/main/java/com/example/applicationouahline/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.applicationouahline;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,18 +17,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void onClickAjouterNoteInptiste(View view){
+        ContentValues values = new ContentValues();
+        values.put(InptisteProvider.NOM, ((EditText)findViewById(R.id.nom)).getText().toString());
+        values.put(InptisteProvider.NOTE, ((EditText)findViewById(R.id.note)).getText().toString());
 
-    public void demarrerService(View view){
-        Intent startIntent = new Intent(this, RingtoneIntentService.class);
-        startIntent.setAction(RingtoneIntentService.ACTION_START_RINGTONE);
-        startService(startIntent);
+        Uri uri = getContentResolver().insert(InptisteProvider.CONTENT_URI, values);
+
+        assert uri != null;
+        Toast.makeText(getBaseContext(), uri.toString(), Toast.LENGTH_LONG).show();
     }
 
-    public void arreterService(View view){
-        Intent stopIntent = new Intent(this, RingtoneIntentService.class);
-        stopIntent.setAction(RingtoneIntentService.ACTION_STOP_RINGTONE);
-        startService(stopIntent);
-    }
+    public void onClickRetrouverNotesInptistes(View view){
+        String URL = "content://com.example.applicationouahline.InptisteProvider";
 
+        Uri inptistes = Uri.parse(URL);
+        Cursor c;
+        c = getContentResolver().query(inptistes, null, null, null, "nom");
+
+        assert c != null;
+        if(c.moveToFirst()){
+            do Toast.makeText(this,
+                    c.getString(c.getColumnIndex(InptisteProvider._ID)) +
+                            ";" + c.getString(c.getColumnIndex(InptisteProvider.NOM)) +
+                            ";" + c.getString(c.getColumnIndex(InptisteProvider.NOTE)),
+                    Toast.LENGTH_SHORT).show();while (c.moveToNext());
+        }
+    }
 
 }
