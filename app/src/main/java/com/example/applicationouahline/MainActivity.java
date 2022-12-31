6c71f1510fde5cbfad975590a6873ad6cbd32b2b
ouahlineomaima <ouahline.omaima@ine.inpt.ma>
2022-12-31 03:02:39 +0100

    Question7

--- app/src/main/java/com/example/applicationouahline/MainActivity.java
+++ app/src/main/java/com/example/applicationouahline/MainActivity.java
@@ -11,11 +11,14 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText nameField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        nameField = findViewById(R.id.nom);
+
     }
     public void onClickAjouterNoteInptiste(View view){
         ContentValues values = new ContentValues();
@@ -42,6 +45,28 @@ public class MainActivity extends AppCompatActivity {
                             ";" + c.getString(c.getColumnIndex(InptisteProvider.NOM)) +
                             ";" + c.getString(c.getColumnIndex(InptisteProvider.NOTE)),
                     Toast.LENGTH_SHORT).show();while (c.moveToNext());
+        }
+    }
+
+    public void onClickRetrouverNotesInptiste(View view){
+        String name = nameField.getText().toString();
+        String URL = "content://com.example.applicationouahline.InptisteProvider";
+
+        Uri inptistes = Uri.parse(URL);
+        Cursor c;
+        String [] l = {name};
+        c = getContentResolver().query(inptistes, null, "nom= ?", l, "nom");
+
+        assert c != null;
+        if(c.moveToFirst()){
+            do Toast.makeText(this,
+                    c.getString(c.getColumnIndex(InptisteProvider._ID)) +
+                            ";" + c.getString(c.getColumnIndex(InptisteProvider.NOM)) +
+                            ";" + c.getString(c.getColumnIndex(InptisteProvider.NOTE)),
+                    Toast.LENGTH_SHORT).show();while (c.moveToNext());
+        }
+        else {
+            Toast.makeText(this, "Etudiant non trouvé", Toast.LENGTH_LONG).show();
         }
     }
 
