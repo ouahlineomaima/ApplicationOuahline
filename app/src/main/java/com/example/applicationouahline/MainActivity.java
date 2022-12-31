f3ec3c03dfe46e62979a32a24db36770bbeacad5
ouahlineomaima <ouahline.omaima@ine.inpt.ma>
2022-12-31 01:10:19 +0100

    Question3-4

--- app/src/main/java/com/example/applicationouahline/MainActivity.java
+++ app/src/main/java/com/example/applicationouahline/MainActivity.java
@@ -1,59 +1,37 @@
 package com.example.applicationouahline;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button createbtn;
+    Button destroybtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "Activité est crée", Toast.LENGTH_SHORT).show();
-        Log.i("onCreate()", "Activité est crée");
+
+        createbtn = findViewById(R.id.createbtn);
+        createbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity2.class);
+                startActivityIfNeeded(intent, 2);
+            }
+        });
+        destroybtn = findViewById(R.id.destroybtn);
+        destroybtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity2.class);
+                startActivity(intent);
+            }
+        });
     }
 
-    @Override
-    protected void onStart(){
-        super.onStart();
-        Toast.makeText(this, "Activité est démarrée", Toast.LENGTH_SHORT).show();
-        Log.i("onStart()", "Activité est démarrée");
-    }
 
-    @Override
-    protected void onRestart(){
-        super.onRestart();
-        Toast.makeText(this, "Activité est redémarrée", Toast.LENGTH_SHORT).show();
-        Log.i("onRestart()", "Activité est redémarrée");
-    }
-
-    @Override
-    protected void onResume(){
-        super.onResume();
-        Toast.makeText(this, "Activité est reprise", Toast.LENGTH_SHORT).show();
-        Log.i("onResume()", "Activité est démarrée");
-    }
-
-    @Override
-    protected void onPause(){
-        super.onPause();
-        Toast.makeText(this, "Activité est en pause", Toast.LENGTH_SHORT).show();
-        Log.i("onPause()", "Activité est en pause");
-    }
-
-    @Override
-    protected void onStop(){
-        super.onStop();
-        Toast.makeText(this, "Activité est stoppée", Toast.LENGTH_SHORT).show();
-        Log.i("onStop()", "Activité est stoppée");
-    }
-
-    @Override
-    protected void onDestroy(){
-        super.onDestroy();
-        Toast.makeText(this, "Activité est détruite", Toast.LENGTH_SHORT).show();
-        Log.i("onDestroy()", "Activity desttroyed");
-    }
 }
