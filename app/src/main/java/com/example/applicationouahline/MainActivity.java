cea5fc773ba9ebf49cefef1a37de66373dd38eef
ouahlineomaima <ouahline.omaima@ine.inpt.ma>
2022-12-31 01:45:24 +0100

    Question 6-7

--- app/src/main/java/com/example/applicationouahline/MainActivity.java
+++ app/src/main/java/com/example/applicationouahline/MainActivity.java
@@ -1,33 +1,38 @@
 package com.example.applicationouahline;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button createbtn;
-    Button destroybtn;
+    Button bouton_implicite, bouton_explicite;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        createbtn = findViewById(R.id.createbtn);
-        createbtn.setOnClickListener(new View.OnClickListener() {
+        bouton_explicite = findViewById(R.id.button);
+        bouton_implicite = findViewById(R.id.button2);
+
+        bouton_explicite.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), MainActivity2.class);
-                startActivityIfNeeded(intent, 2);
+                Intent intent = new Intent(getBaseContext(), MainActivity2.class);
+                intent.putExtra("nom", "ouahline");
+                intent.putExtra("prénom", "omaima");
+                startActivity(intent);
             }
         });
-        destroybtn = findViewById(R.id.destroybtn);
-        destroybtn.setOnClickListener(new View.OnClickListener() {
+
+        bouton_implicite.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), MainActivity2.class);
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("http://www.inpt.ac.ma"));
                 startActivity(intent);
             }
         });
